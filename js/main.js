de6dfe7afb0d75b5ef58d128538cd33eb545072d
joeramirez07 <joeramirez07@gmail.com>
2025-04-28 14:55:48 -0500

    init

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 
 
   /*----- state variables -----*/
+let secretWord = '';
+let currentGuess = '';
+let currentRow = 0;
+let gameOver = false;
 
 
   /*----- cached elements  -----*/
@@ -11,3 +15,20 @@
 
 
   /*----- functions -----*/
+// Initialize the game
+//Declare, but don't initialize, the application-wide state variables. 
+// The initialization of the variables to their "initial" state should be done within an initialize, 
+// //or similarly named function, e.g., init.
+function init() {
+    secretWord = WORDS[Math.floor(Math.random() * WORDS.length)].toUpperCase();
+    currentGuess = '';
+    currentRow = 0;
+    gameOver = false;
+  
+    buildBoard();
+  }
+  
+  function pickSecretWord()[
+    const randomIndex = Math.floor(Math.random) *  
+
+  ]
